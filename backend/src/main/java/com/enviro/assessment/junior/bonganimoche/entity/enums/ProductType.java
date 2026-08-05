cce935e11034d0749799a421079db70e6f8213f7
Bongani Moche <bonganimoche8@gmail.com>
2026-08-05 13:02:58 +0200

    feat: add domain entities and enums
    - Add Investor entity doubling as the authentication principal
    - Add Product entity with stored BigDecimal balance (precision 19, scale 2)
    - Add WithdrawalNotice with balance snapshots for auditable statements
    - Add ProductType and WithdrawalStatus enums mapped as STRING
    - Derive investor age from date of birth for the retirement rule

---
+++ backend/src/main/java/com/enviro/assessment/junior/bonganimoche/entity/enums/ProductType.java
@@ -0,0 +1,11 @@
+package com.enviro.assessment.junior.bonganimoche.entity.enums;
+
+/**
+ * Distinguishes the two investment product categories.
+ * RETIREMENT is subject to the age > 65 withdrawal restriction;
+ * SAVINGS has no age restriction.
+ */
+public enum ProductType {
+    SAVINGS,
+    RETIREMENT
+}
